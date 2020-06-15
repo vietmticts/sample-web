7d1de42b07f099e9f0c5ee30163c8d36d5f04649
nguyendv <nguyendv@icts.vn>
2020-06-15 14:26:18 +0700

    Updates

---
+++ precache-manifest.fd9a19d7f78cf7280de1a254f85fb9eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145eab4fb892e3c31949bb79130a116e",
+    "url": "/sample-web/sample-web/index.html"
+  },
+  {
+    "revision": "f840512386580069551e",
+    "url": "/sample-web/sample-web/static/css/main.e97b4ad6.chunk.css"
+  },
+  {
+    "revision": "caa798ef167eaeb5dbf7",
+    "url": "/sample-web/sample-web/static/js/2.456bbb19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-web/sample-web/static/js/2.456bbb19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f840512386580069551e",
+    "url": "/sample-web/sample-web/static/js/main.6854ff44.chunk.js"
+  },
+  {
+    "revision": "ffdf334de6b56f818cf7",
+    "url": "/sample-web/sample-web/static/js/runtime-main.85c0d69d.js"
+  }
+]);
