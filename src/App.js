f40745534107f4f3e7712df0eb5cc330bf32554a
nguyendv <nguyendv@icts.vn>
2020-06-15 13:45:50 +0700

    implement tel, sms... links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Default iOS Schemes </p>
+        <a className="App-link" href="tel:091236029" >tel:091236029</a>
+        <a className="App-link" href="sms:091236029" >sms:091236029</a>
+        <a className="App-link" href="sms:" >sms:</a>
+        <a className="App-link" href="facetime:091236029" >facetime:091236029</a>
+        <a className="App-link" href="facetime-audio:091236029" >facetime-audio:091236029</a>
+        <a className="App-link" href="mailto:tester@icts.vn" > Mail to tester@icts.vn </a>
+        <a className="App-link" href="mailto:" > Mail </a>
+        
+        <a className="App-link" href="facetime:tester@icts.vn" >facetime:tester@icts.vn</a>
+        <a className="App-link" href="facetime-audio:tester@icts.vn" >facetime-audio:tester@icts.vn</a>
+        {/* <a className="App-link" href="http://maps.apple.com/?ll=50.894967,4.341626" > Map </a> */}
       </header>
     </div>
   );
